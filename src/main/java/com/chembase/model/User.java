a82f2d60a02ff002bd84ae24d7830113ca158258
mmeziane <mmeziane159@gmail.com>
2024-10-01 21:33:10 +0200

    First commit

---
+++ src/main/java/com/chembase/model/User.java
@@ -0,0 +1,28 @@
+package com.chembase.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+
+    @ManyToMany
+    @JoinTable(
+            name = "user_group",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "group_id")
+    )
+    private Set<Group> groups;
+}
