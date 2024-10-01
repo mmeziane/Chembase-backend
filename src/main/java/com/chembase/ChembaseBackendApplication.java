be078a89a640e4b535afabff77944d2f8e7ff410
mmeziane <mmeziane159@gmail.com>
2024-10-01 11:02:42 +0200

    first commit

---
+++ src/main/java/com/chembase/ChembaseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.chembase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChembaseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChembaseBackendApplication.class, args);
+	}
+
+}
