951f03151c26a4647e7174b004ab6a1e36b26fc1
mmeziane <mmeziane159@gmail.com>
2024-10-02 21:44:22 +0200

    Add JwtAuthenticationResponse class for handling JWT access tokens

---
+++ src/main/java/com/chembase/security/JwtAuthenticationResponse.java
@@ -0,0 +1,18 @@
+package com.chembase.security;
+
+public class JwtAuthenticationResponse {
+    private String accessToken;
+    private String tokenType = "Bearer";
+
+    public JwtAuthenticationResponse(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public String getTokenType() {
+        return tokenType;
+    }
+}
